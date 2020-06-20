611ef37a30a5cf78fbd87ec73109d3cedf016f62
dominicmcginnis <dominic_mcginnis@yahoo.com>
2020-06-19 22:13:36 -0700

    Adding in terminal default text and fixing dpi

--- js/terminal.js
+++ js/terminal.js
@@ -1,9 +1,35 @@
 let canvas, ctx;
+let terminalStr = '(Passkey) ~$ '
+let textPosX, textPosY;
+//get DPI
+let dpi = window.devicePixelRatio;
 
 function init () {
-  canvas = document.getElementById('terminal');
-  ctx = canvas.getContext('2d');
-  
+  	canvas = document.getElementById('terminal');
+  	ctx = canvas.getContext('2d');
+  	fix_dpi();
+  	textPosX = 0;
+  	textPosY = canvas.height - 3;
+  	ctx.save();
+	ctx.fillStyle = 'white';
+	// text specific styles
+	ctx.font = '20px Inconsolata, monospace';
+	ctx.textAlign = 'left';
+	ctx.textBaseline = 'alphabetic';
+	ctx.fillText(terminalStr, textPosX, textPosY);
+	ctx.restore();
+}
+//Thanks to https://medium.com/wdstack/fixing-html5-2d-canvas-blur-8ebe27db07da for solution
+function fix_dpi() {
+	//get CSS height
+	//the + prefix casts it to an integer
+	//the slice method gets rid of "px"
+	let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+	//get CSS width
+	let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+	//scale the canvas
+	canvas.setAttribute('height', style_height * dpi);
+	canvas.setAttribute('width', style_width * dpi);
 }
 
 document.addEventListener('DOMContentLoaded', init);
