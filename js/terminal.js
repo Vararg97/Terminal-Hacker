0aacd63b617b234dba02043cf3114b2ae672c6a8
Cole McGinnis <colemcginnis@Coles-MacBook-Air.local>
2020-06-20 08:09:49 -0700

    a

--- js/terminal.js
+++ js/terminal.js
@@ -4,6 +4,9 @@ function init () {
   canvas = document.getElementById('terminal');
   ctx = canvas.getContext('2d');
   
+  ctx.fillStyle = "white";
+  ctx.font = "10px Inconsolata, monospace";
+ctx.fillText("Hello World", 10, 30);
 }
 
 document.addEventListener('DOMContentLoaded', init);
